f2ef7c190fffd1a104bbe2b2afc751979ee84e73
BldNst <148994081+BldNst@users.noreply.github.com>
2024-06-15 21:47:07 +0700

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -15,7 +15,9 @@ SECRET_KEY = 'django-insecure-*@3tyf=&*(g0=_!=^nm@q-u398tc)jte(t3j%uioe@3@ra7a_1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['52.41.36.82',
+'54.191.253.12',
+'44.226.122.3']
 
 
 # Application definition
